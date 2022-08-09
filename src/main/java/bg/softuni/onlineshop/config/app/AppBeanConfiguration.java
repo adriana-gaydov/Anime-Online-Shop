8dfa280e6f67cd8d2a9620eea10a9da70f35dddd
Adriana Gaydova <86288896+adriana-gaydova@users.noreply.github.com>
2022-08-09 18:48:53 +0300

    Cloudinary removed info.

--- src/main/java/bg/softuni/onlineshop/config/app/AppBeanConfiguration.java
+++ src/main/java/bg/softuni/onlineshop/config/app/AppBeanConfiguration.java
@@ -14,9 +14,9 @@ public class AppBeanConfiguration {
     public Cloudinary cloudinaryConfig() {
         Cloudinary cloudinary = null;
         Map config = new HashMap();
-        config.put("cloud_name", "adrianapersonalprj");
-        config.put("api_key", "628952652338638");
-        config.put("api_secret", "fvXunCZTU2CDU-J3oM_TxmTxizc");
+        config.put("cloud_name", "YOUR VAL");
+        config.put("api_key", "YOUR VAL");
+        config.put("api_secret", "YOUR VAL");
         cloudinary = new Cloudinary(config);
         return cloudinary;
     }
